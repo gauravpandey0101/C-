08eeb5b5b42bf6d75f70bfec64a5056f51c6a320
Gaurav Pandey <107850615+gauravpandey0101@users.noreply.github.com>
2022-08-14 16:27:36 +0530

    Add files via upload

---
+++ pattern4.cpp
@@ -0,0 +1,27 @@
+/*
+    Here we made this pattern  -    A B C
+                                    A B C
+                                    A B C
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i=1,n;
+    cin >> n;
+
+    while (i <= n)
+    {
+        int j=1;
+        while(j<=n)
+        {   
+            char ch='A'+j-1;
+            cout <<" " << ch;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+}
