60a9140ef832758311620ef28bcd7a5798cc47ad
Gaurav Pandey <107850615+gauravpandey0101@users.noreply.github.com>
2022-08-25 23:00:38 +0530

    Add files via upload

---
+++ pattern_01.cpp
@@ -0,0 +1,24 @@
+/*
+    1 3 5 7
+    3 5 7 1
+    5 7 1 3
+    7 1 3 5
+*/
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int r,c,n=4;
+    
+    for(r=1; r <= (n*2-1); r++)
+    {
+        for(c=r; c <= (n*2-1); c++)
+        {
+            cout << c;
+        }
+            cout << endl;
+    }
+
+    return 0;
+}
