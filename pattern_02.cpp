4eec0087871ca2c8d5ad8f3a531ebbdde0b7e617
Gaurav Pandey <107850615+gauravpandey0101@users.noreply.github.com>
2022-08-25 23:00:55 +0530

    Add files via upload

---
+++ pattern_02.cpp
@@ -0,0 +1,34 @@
+/* 
+          1
+        2 3 2
+      3 4 5 4 3
+    4 5 6 7 6 5 4
+*/
+
+#include<iostream>
+using namespace std;
+
+int
+main ()
+{
+  int r, c, s, n = 4, k = 1;
+
+  //  For Row 
+  for (r = 1; r <= n; r++)
+    {
+      //  For Space
+      for (s = r; s < n; s++)
+	{
+	  cout << " ";
+	}
+	//  For 1st Number 
+	int t=r;
+      for (c = 1; c <= r; c++)
+	{
+	  cout << t++;
+	}
+	//  For 2st Number
+      cout << endl;
+    }
+  return 0;
+}
